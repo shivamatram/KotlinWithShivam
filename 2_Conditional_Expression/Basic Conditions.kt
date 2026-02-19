1b39602f343678f87b960217b6e16200faabd5ef
Shivam Atram <shivamatram20002@gmail.com>
2026-02-20 02:31:23 +0530

    Basic conditions and loops

--- 2_Conditional_Expression/Basic Conditions.kt
+++ 2_Conditional_Expression/Basic Conditions.kt
@@ -1,5 +1,47 @@
-package com.example.kotlinseriestrial.`2_Conditional_Expression`
+package com.example.kotlinseriestrial
+
+import com.sun.jdi.IntegerType
 
 
 // if and else basic syntax
+//fun main(){
+//    val age = readln().toInt()
+//    if(age < 10){
+//        println("You can not go near to the bicycle")
+//    }else if(age < 18){
+//        println("You can not ride bicycle")
+//    }else{
+//        println("You can ride bicycle")
+//    }
+//}
 
+//fun main(){
+//    val age : Int? = null
+//    println("Age : $age")
+//}
+
+//fun main(){
+//    println("enter an number to get the day associated to it : ")
+//    val day : Int = readln().toInt()
+//    when(day){
+//        1 -> println("Today is monday")
+//        2 -> println("Today is tuesday")
+//        3 -> println("Today is wednesday")
+//        4 -> println("Today is thursday")
+//        5 -> println("Today is friday")
+//        6 -> println("Today is saturday")
+//        7 -> println("Today is sunday ( weekend day)")
+//        else -> println("Invalid number inter the number between 1 to 7")
+//    }
+//}
+
+//fun main(){
+//    println("Enter your age : ")
+//    val age : Int = readln().toInt()
+//    var max : Int? = if (age < 18) 2 else 1
+//    if(max == 2){
+//        println("You are not an adult, you are child")
+//    }else{
+//        println("You are an adult")
+//    }
+//}
--- 3_Loops/Loop.kt
+++ 3_Loops/Loop.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinseriestrial.`3_Loops`
+package com.example.kotlinseriestrial
 
 
 /*
@@ -21,6 +21,30 @@ for (variable in range/collection) {
 
 // Example
 
+//fun main(){
+//    for(i in 1..10){
+//        println("current number is $i")
+//    }
+//}
+
+//fun main(){
+//    val arr = Array<Int>(5) {0}
+//    for(i in 0 until arr.size){
+//        arr[i] = readln().toInt()
+//    }
+//    for(i in arr.indices){
+//        println("$i is ${arr[i]}")
+//    }
+//    val arr = IntArray(5){
+//        readln().toInt()
+//    }
+//    println(arr.joinToString())
+//    val array : Array<Int> = arrayOf(1,2,3,4,5)
+//    val array1 = arrayOf(1,2,3,4,"Shivam",1.0)
+//    for(j in array1.indices){
+//        println(array1[j])
+//    }
+//}
 //fun main() {
 //
 //    for (j in 1..5){
@@ -39,7 +63,24 @@ for (variable in range/collection) {
 //        println(fruit)
 //    }
 //}
+//fun main() {
+//    val fruits = List(5) {
+//        readln().toInt()
+//    }
+//    println(fruits.joinToString())
+//}
+//fun main(){
+//    val fruits = listOf("apple", "orange", "apple")
+//    for (f in fruits){
+//        println(f)
+//    }
+//    println(fruits.joinToString())
 
+//    val list = lazy { readln().toInt() }
+//    for(i in 0 until 5){
+//        println(i)
+//    }
+//}
 
 
 /*
@@ -48,8 +89,18 @@ for (variable in range/collection) {
 while (condition) {
     // Code to execute while the condition is true
 }
-
  */
+//fun main() {
+//    println("Enter any number : ")
+//    val number = readln().toInt()
+//    var i = 1
+//    while (i <= number) {
+//        if(i % 2 != 0) {
+//            print(i.toString() + " ")
+//        }
+//        i++
+//    }
+//}
 
 
 
